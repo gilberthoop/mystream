e14bd50b2c2b15ce39386891c065fbd87f5830fe
gilberthoop <williamgilbertgo@gmail.com>
2020-08-30 12:42:50 -0700

    Added history object to do programmatic navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -28,8 +29,9 @@ export const createStream = formValues => async (dispatch, getState) => {
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
 
-    // TODO: Programmatic navigation to get the user back
+    // Programmatic navigation to get the user back
     // to the root route
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
